b528b55fdee986d2273456b7f3521fe8e1582395
Debarchito Nath <debarchiton@proton.me>
2026-07-12 19:16:26 +0530

    refactor(flake): port flake to flake-parts
    migrate to flake-parts for 1:1 parity and to fix evaluation breaks on non-Darwin systems after NixOS 26.11 dropped x86_64-darwin support

--- nix/package.nix
+++ nix/package.nix
@@ -3,49 +3,54 @@
   bun2nix,
   lib,
   ...
-}: let
+}:
+let
   packageJson = lib.importJSON ../package.json;
 in
-  bun2nix.mkDerivation {
-    pname = "hunkdiff";
-    version = packageJson.version;
+bun2nix.mkDerivation {
+  pname = "hunkdiff";
+  version = packageJson.version;
 
-    src = ../.;
+  src = ../.;
 
-    bunDeps = bun2nix.fetchBunDeps {
-      bunNix = ./bun.lock.nix;
-    };
+  bunDeps = bun2nix.fetchBunDeps {
+    bunNix = ./bun.lock.nix;
+  };
 
-    buildPhase = ''
-      runHook preBuild
-      mkdir -p .bun-tmp .bun-install
-      BUN_TMPDIR=$PWD/.bun-tmp \
-      BUN_INSTALL=$PWD/.bun-install \
-      ${bun}/bin/bun build --compile \
-        --no-compile-autoload-bunfig \
-        "./src/main.tsx" \
-        --outfile "hunk-bin"
-      runHook postBuild
-    '';
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p .bun-tmp .bun-install
+    BUN_TMPDIR=$PWD/.bun-tmp \
+    BUN_INSTALL=$PWD/.bun-install \
+    ${bun}/bin/bun build --compile \
+      --no-compile-autoload-bunfig \
+      "./src/main.tsx" \
+      --outfile "hunk-bin"
+    runHook postBuild
+  '';
 
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/bin
-      cp -p ./hunk-bin $out/bin/hunk
-      cp -r ./skills $out/
-      runHook postInstall
-    '';
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp -p ./hunk-bin $out/bin/hunk
+    cp -r ./skills $out/
+    runHook postInstall
+  '';
 
-    # See https://nix-community.github.io/bun2nix/building-packages/hook.html#arguments for options
-    dontFixup = true;
-    dontStrip = true;
-    dontRunLifecycleScripts = true;
+  # See https://nix-community.github.io/bun2nix/building-packages/hook.html#arguments for options
+  dontFixup = true;
+  dontStrip = true;
+  dontRunLifecycleScripts = true;
 
-    meta = with lib; {
-      description = "Terminal diff viewer for agentic changesets";
-      homepage = "https://github.com/modem-dev/hunk";
-      license = licenses.mit;
-      mainProgram = "hunk";
-      platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    };
-  }
+  meta = with lib; {
+    description = "Terminal diff viewer for agentic changesets";
+    homepage = "https://github.com/modem-dev/hunk";
+    license = licenses.mit;
+    mainProgram = "hunk";
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+  };
+}
